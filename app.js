881271c7f26b0d7a979b7c170a4671288dcf6771
israelurcola <urcolaisrael@gmail.com>
2023-07-09 12:31:52 -0300

    trabaje sobre el app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const app = express();
+
+const path = require('path'); 
+app.use(express.static('public'));
+
+
+app.get('/',function(req,res) {
+    res.send("lero lero");
+});
+
+app.get('/home', (req,res) => {
+    res.sendFile (path.join(__dirname, '/views/home.html'))});
+
+
+
+app.listen(3030 ,() => console.log('sevidor corriendo'));
